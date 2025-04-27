73e5bafd4fa238a834a363ffb539403862789abb
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-04-26 22:00:12 -0300

    sim

---
+++ Faculdade - Python Básico/atividadeArrays/primeiroscontatos.py
@@ -0,0 +1,23 @@
+ListCode = [1, 2, 3, 4, 5, 6]
+ListFood = ["Hot-dog", "X-Salada", "X-Egg", "X-Tudo", "Refrigerante", "Água"]
+ListPrices = [25.00, 30.00, 35.00, 50.00, 08.00, 05.00]
+
+Confirm = input("Deseja fazer um pedido (S/N)?\n")
+
+if ((Confirm == "S") or (Confirm == "s")):
+    
+    print("\n")
+
+    Name = input("Digite o seu nome, por favor: ")
+
+    print("\n")
+
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])                       
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])               
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])   
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])
+    print(ListCode[0], " - ", ListFood[0], " - " "R$ ", ListPrices[0])
+
+    print("Escolha a opção de desejada\n")
+    if 
