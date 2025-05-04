fe0ed6071d848437b32eff3ab00730eead514f79
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-04 18:29:14 -0300

    s

---
+++ Faculdade - Python Básico/atividadeRepet/Atividades.py
@@ -0,0 +1,8 @@
+print("Atividade 1: Exercícios - Estrutura de Repetição - PARA (atividade do dia 24/04)\n")
+
+print("1)Elaborar um algoritomo que leia um valor de tempratura em graus Celsius, calcule e exiba a temperatura equivalente em Kelvin, sabendo que: K = C = 273. Esse algoritmo deve repetir 5 vezes.\n")
+
+
+for n in range (5):
+cel = Flot(p  digite a temperatura em graus Celsius"))
+
