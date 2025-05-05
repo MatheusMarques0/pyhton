c69dc930f5f7b7c64daeaf7a493ec7bf72c5c3b3
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-04 21:19:58 -0300

    sss

---
+++ Faculdade - Python Básico/atividadeRepet/Atividade5.py
@@ -0,0 +1,16 @@
+print("Atividade 1: Exercícios - Estrutura de Repetição - PARA (atividade do dia 24/04)\n")
+
+print("5)Elaborar um algoritmo que soicite ao usuário para digitar 15 valores e deve exibir a soma e média\n")
+
+valores = []
+
+for n in range (15):
+    val = float(input("Digite um valor para ser usado\n"))
+    valores.append(val)
+    print(valores)
+
+soma = sum(valores)
+
+media  = soma / 15
+
+print(f"a soma dos números é {soma}, e a média é {media}")
