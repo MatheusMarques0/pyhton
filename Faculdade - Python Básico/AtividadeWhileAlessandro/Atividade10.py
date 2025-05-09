b6f623fd1803c3df2ba794f556b981ab2c2be88b
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-08 23:50:23 -0300

    sim

--- Faculdade - Python Básico/AtividadeWhileAlessandro/Atividade10.py
+++ Faculdade - Python Básico/AtividadeWhileAlessandro/Atividade10.py
@@ -4,34 +4,16 @@ print("10) Elaborar um programa que contém uma lista com 15 elementos. Essa lis
 
 unique_list = []
 
+normal_list = []
+
 while len(unique_list) < 15:
-    entrada = input("Digite um número (números repetidos não serão computados): ")
+    num = float(input("Digite um número, se for repetido não será contado"))
+    normal_list.append(num)
 
-    i = 0
-    e_valido = 1
-    while i < len(entrada):
-        c = entrada[i]
-        if not (c.isdigit() or c == '.' or (i == 0 and c == '-')):
-            e_valido = 0
-        i += 1
+    unique = set(normal_list)
 
-    if e_valido == 0:
-        print("Entrada inválida. Por favor, digite um número.")
-    else:
-        num = float(entrada)
+    if len(unique) == 15:
+        unique_list.append(unique)
+        break
 
-        i = 0
-        repetido = 0 
-        while i < len(unique_list):
-            if unique_list[i] == num:
-                repetido = 1
-            i += 1
-
-        if repetido == 1:
-            print("Número repetido! Tente outro.")
-        else:
-            unique_list.append(num)
-
-print("\nLista final com 15 números únicos:")
 print(unique_list)
-
