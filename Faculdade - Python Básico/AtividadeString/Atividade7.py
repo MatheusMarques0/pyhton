3fefa4b1fad05ce55bd852e5ca1953e3c43e62dd
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-14 20:55:23 -0300

    s

--- Faculdade - Python Básico/AtividadeString/Atividade7.py
+++ Faculdade - Python Básico/AtividadeString/Atividade7.py
@@ -1,2 +1,10 @@
 print("Exercícios - String")
 
+print("Elaborar um programa que, dada uma string, diga se ela é um palíndromo ou não. Lembrando que um palíndromo é uma palavra que tenha a propriedade de poder ser lida tanto da direita para a esquerda como da esquerda para a direita (Exemplo: ovo, ana). \n")
+
+palavra = str(input("Digite um nome: \n"))
+
+if palavra == palavra[::-1]:
+    print(f"A palavra {palavra} é um palíndromo.")
+else:
+    print(f"A palavra {palavra} não é um palindromo.")
