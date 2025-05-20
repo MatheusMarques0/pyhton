f366ef9c39f5350e90914cfd171ed8d4935aa369
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-20 17:58:56 -0300

    Create exe1.py

---
+++ Faculdade - Python Básico/AtividadeTXT/exe1.py
@@ -0,0 +1,15 @@
+print("1) Desenvolva um algoritmo que escreve em disco um arquivo com números ordenados crescentemente de 1 a 100. Cada número deve ser separado por ','. O arquivo deve se chamar 'crescente.txt'. \n")
+
+lista = []
+
+arquivo = open("AtividadeTXT/crescente.txt", "w")
+
+for n in range(100):
+    if n < 99:
+        arquivo.write(str(n + 1) + ";")
+    else:
+        arquivo.write(str(n + 1) + ".")
+
+arquivo.close()
+
+print("Arquivo gerado.")
