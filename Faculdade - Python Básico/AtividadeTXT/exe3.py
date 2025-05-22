428b005e38b37bc69f56f2aee16d843805fabca1
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-05-21 21:47:43 -0300

    .

--- Faculdade - Python Básico/AtividadeTXT/exe3.py
+++ Faculdade - Python Básico/AtividadeTXT/exe3.py
@@ -16,8 +16,8 @@ def menu(): #Menu, ele será a primeira coisa a ser chamada, retorna um input
     print("3) Buscar aluno")
     print("4) Remover aluno")
     print("5) Sair")
-    option = str(input("Digite qual opção deseja (escreva um número de 1 a 5)."))
-    while option == "" and option != 1 and option != 2 and option != 3 and option != 4 and option != 5:
+    option = str(input("Digite qual opção deseja (escreva um número de 1 a 5).")).strip()
+    while option == "" and option != "1" and option != "2" and option != "3" and option != "4" and option != "5":
         option = str(input("Por favor, digite um número correto (de 1 a 5): "))
 
     return option
@@ -36,14 +36,16 @@ def cadastro(): #Abre arquivo, pega nome, coloca no arquivo, repita.
 def listar(): #Essa foi complicada.
     arquivo = open("C:/Users/renat/OneDrive/Documentos/estudos/pyhton/Faculdade - Python Básico/AtividadeTXT/cadastro.txt", "r")
     listar = arquivo.readlines() #Irá ler linha por linha
-    tamanholistar = len(listar) #Tamanho do arquivo
-    for n in range(tamanholistar): #Ponteiro para a repetição
-        linha = listar[0] #Pega o primeiro termo da lista (o primeiro cadastro, no caso), inicialmente eu estava com problemas por colocar [listar[0]] com duas chaves [], mas pelo visto isso torna linha uma lista, e listas não podem usar .find(), algo para se anotar.
-        start_fulano = linha.find("nome:") + len("nome:") #Acha a palavra nome, tem indice 0, por isso coloquei + len("nome"), quero saber o nome do aluno, não "nome:fulano". Poderia ter colocado só 5 também, mas ficaria menos escalavel.
-        end_fulano = linha.find("e-mail:") #Vai achar o índice do email, ou seja, quando ele começa
-        name = linha[start_fulano:end_fulano].strip() #O professor ensinou esse método em sala da aula, ele vai do ponto A até o B da lista, no caso, onde começa o nome e onde começa o email. Eu não sei se o professor ensinou o strip em sala de aula, mas eu vi isso naquele desafio da Erika junto com o Luigi :D
-        print(name) #Vai retornar o nome do aluno
     arquivo.close()
+
+    if len(listar) == 0:
+        print("Nenhum aluno cadastrado.")
+    else:
+        for linha in listar: #Ponteiro para a repetição
+            start_fulano = linha.find("nome:") + len("nome:") #Acha a palavra nome, tem indice 0, por isso coloquei + len("nome"), quero saber o nome do aluno, não "nome:fulano". Poderia ter colocado só 5 também, mas ficaria menos escalavel.
+            end_fulano = linha.find("e-mail:") #Vai achar o índice do email, ou seja, quando ele começa
+            name = linha[start_fulano:end_fulano].strip() #O professor ensinou esse método em sala da aula, ele vai do ponto A até o B da lista, no caso, onde começa o nome e onde começa o email. Eu não sei se o professor ensinou o strip em sala de aula, mas eu vi isso naquele desafio da Erika junto com o Luigi :D
+            print(name) #Vai retornar o nome do aluno
 
 def buscar():
     arquivo = open("C:/Users/renat/OneDrive/Documentos/estudos/pyhton/Faculdade - Python Básico/AtividadeTXT/cadastro.txt", "r")
@@ -55,7 +57,7 @@ def buscar():
 
     for linha in listar: #Vai procurar na linha toda
         if f"nome:{searchFulano}" in linha: #Se o nome estiver em linha: encontrado
-            print("Aluno encontrado!")
+            print("Aluno encontrado! \n")
             print(linha)
             encontrado = True #Se verdadeiro, o encontrado se torna True e passa direto pelo if not
             break
@@ -64,13 +66,43 @@ def buscar():
         print("Aluno não encontrado.")
 
 def remove():
-    sim #a fazer
+    arquivo = open("C:/Users/renat/OneDrive/Documentos/estudos/pyhton/Faculdade - Python Básico/AtividadeTXT/cadastro.txt", "r")
+    listar = arquivo.readlines()
+    delete = str(input("Digite o nome do aluno que você quer deletar: ")).strip()
+    encontrado = False 
+    nova_linhas = []
+
+    for linha in listar:
+        if f"nome:{delete}" in linha:
+            print("Aluno encontrado! \n")
+            sure = str(input("Você tem certeza que deseja apagar o usuário? (s/n): ")).strip().lower()
+            if sure == "s":
+                print("aluno removido com sucesso \n")
+                encontrado = True
+            else:
+                print("Compreensível, tenha um bom dia. \n")
+                nova_linhas.append(linha)
+
+        else:
+            nova_linhas.append(linha)
+
+    if not encontrado:
+        print("Aluno não encontrado. \n")
+
+    arquivo = open("C:/Users/renat/OneDrive/Documentos/estudos/pyhton/Faculdade - Python Básico/AtividadeTXT/cadastro.txt", "w")
+    arquivo.writelines(nova_linhas)
+
+    arquivo.close()
 
 #Main
 
 while confirm == "s":
 
     escolha = menu() # mostrará o menu, e o resultado virará uma variável
+
+    while escolha != "1" and escolha != "2" and escolha != "3" and escolha != "4" and escolha != "5":
+        print("[ERRO] Dígito inválido \n")
+        escolha
 
     if escolha == "1": #dependendo da escolha o comando irá fazer algo
         print("A opção desejada foi fazer o cadastro de um novo aluno \n")
