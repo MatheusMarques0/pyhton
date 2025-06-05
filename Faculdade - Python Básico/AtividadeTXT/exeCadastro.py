c3fabad40218ba1d7d226a48538850046a40a869
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-06-04 21:54:31 -0300

    .

--- Faculdade - Python Básico/AtividadeTXT/exeCadastro.py
+++ Faculdade - Python Básico/AtividadeTXT/exeCadastro.py
@@ -13,7 +13,11 @@ temp = []
 dados_temp = []
 temp_cod = []
 lista_produto = [] # função listar, 87 [codígos]
-codigo_produto = [] #código dos pedidos, função listar
+codigo_produto = [] # código dos pedidos, função listar
+ 
+lista_pedido = [] # lista de pedidos do cliente, será usado em um segundo txt
+
+totalprice = [] #lista que armazenará os preços dos produtos comprados(já multiplicado pela quantia desejada) será usado para dizer o preço total da compra
  
 #funções
  
@@ -42,17 +46,18 @@ def menuADM():
     return optionAdm
  
 def CadastrarADM():
-
+ 
     temp.clear()
     temp_cod.clear()
-    
+   
     print("\nA opção desejada foi de cadastrar no modo ADM")
     codigo = 0
     arquivo = open("cadastroADM.txt", 'r', encoding='utf-8')
     dados = arquivo.readlines()
-
+ 
     if dados == []:
         codigo = 100
+        print("teste")
         arquivo.close()
     else:
         for i in range(len(dados)):
@@ -60,7 +65,7 @@ def CadastrarADM():
             dados[i] = dados[i].split(';')
             temp.append(dados[i][0])
         arquivo.close()
-
+ 
         for p in range(len(temp)):
             temp[p] = int(temp[p])  
         numeros = set(temp)
@@ -72,19 +77,19 @@ def CadastrarADM():
             for numero in faltantes:
                 temp_cod.append(numero)
             codigo = str(temp_cod[0])
-
+ 
     print(f"Código do produto: {codigo}")
-
+ 
     description = str(input("Descreva o arquivo a ser computado: "))
     while (description == ""):
         print("Valor inválido \n")
         description = str(input("Descreva o arquivo a ser computado: "))
-
+ 
     price = str(input("Digite qual será o preço do produto: "))
     while (price == "" or price.isalpha()):
         print("Valor inválido! \n")
         price = str(input("Digite qual será o preço do produto: "))
-
+ 
     arquivo = open("cadastroADM.txt", 'a+', encoding='utf-8')
     if dados == []:
         arquivo.write(f"{codigo}" + ";" + description + ";" + price)
@@ -96,7 +101,7 @@ def CadastrarADM():
         for g in range (len(lista_cad)):
             arquivo.write(str(lista_cad[g][0]) + ";" + str(lista_cad[g][1]) + ";" + str(lista_cad[g][2]) + "\n")
         arquivo.close()
-
+ 
     print("Arquivo gerado com sucesso!")
  
 def ListarADM():
@@ -117,7 +122,7 @@ def EditarADM():
     ListarADM() #chamando a função, reduzindo código
  
     arquivo = open("cadastroADM.txt", "r", encoding='utf-8')
-
+ 
     print("A opção desejada foi Editar como ADM\n")
          
     cod_alterar = str(input("Digite o código do produto que deseja alterar: "))
@@ -126,7 +131,7 @@ def EditarADM():
  
     codigo_changer = codigo_produto.index(cod_alterar)
     print("\nA descrição atual do produto é: %s" %(lista_produto[codigo_changer][1])) #amém, eu tive que criar 2 listas para que ess método funcionasse, uma com os código e uma com os pedidos
-
+ 
     new_description = str(input("Digite a nova descrição do produto: "))
     while (new_description == ""):
         new_description = str(input("[ERRO] Não deixe o espaço vazio, digite uma opção válida: "))
@@ -152,7 +157,7 @@ def EditarADM():
  
 def ExcluirADM():
     ListarADM() #chamando a função, reduzindo código
-
+ 
     print("A opção desejada foi de Excluir como ADM\n")
          
     cod_erase = str(input("Digite o código do produto que deseja excluir: "))
@@ -160,56 +165,88 @@ def ExcluirADM():
         cod_erase = str(input("[ERRO], por favor digite um código que exista: "))
  
     codigo_changer = codigo_produto.index(cod_erase)
-    print("\nO produto selecionado tem o código: %s. Descrição: %s. Preço: %s \n" %(lista_produto[codigo_changer][0], lista_produto[codigo_changer][1], lista_produto[codigo_changer][2])) 
+    print("\nO produto selecionado tem o código: %s. Descrição: %s. Preço: %s \n" %(lista_produto[codigo_changer][0], lista_produto[codigo_changer][1], lista_produto[codigo_changer][2]))
  
     conf = str(input("Tem certeza que deseja apagar o produto? (1 = 'sim' / 2 = 'não'): "))
     while (conf != "1" and conf != "2"):
         conf = str(input("[ERRO] por favor digite uma opçã válida (1 ou 2): "))
-
+ 
     if conf == "1":
         lista_produto.pop(codigo_changer)
         codigo_produto.pop(codigo_changer)
-
+ 
         with open("cadastroADM.txt", "w", encoding="utf-8") as arquivo: #se não colocar "with open" o 'arquivo' da erro
             for n in range(len(lista_produto)):
                 arquivo.write(lista_produto[n][0] + ";" + lista_produto[n][1] + ";" + lista_produto[n][2] + "\n")
     else:
         print("Ação Cancelada! \n")
-
-
+ 
+ 
 def operator():
+    choise = 1
     ListarADM()
-
-    choose = str(input("Digite o código do produto que deseja comprar: "))
-    while choose not in codigo_produto:
-        choose = str(input("[ERRO] por favor digite um número válido: "))
-
-    codigo_choosed = codigo_produto.index(choose)
-    print("\nO produto selecionado tem o código: %s. Descrição: %s. Preço: %s \n" %(lista_produto[codigo_choosed][0], lista_produto[codigo_choosed][1], lista_produto[codigo_choosed][2]))#Como é bom reaproveitar código...Quero só ver na prova :D
-
-    quantidade = int(input("Digite a quantidade que deseja do produto: "))
-    while quantidade <= 0:
-        quantidade = int(input("[EROO] Digite uma quantidade VÁLIDA para o produto: "))
-
-    realmoney = float(lista_produto[codigo_choosed][2])
-
-    despesa = realmoney * quantidade
-
-    print("O total será de %0.2f \n" % (despesa))
-
-    print("Pedido do Cliente: Código: %s.  Descrição: %s. Preço: %s. Total a pagar: %0.2f \n" %(lista_produto[codigo_choosed][0], lista_produto[codigo_choosed][1], lista_produto[codigo_choosed][2], despesa))
-
-    conf = str(input("Deseja confirmar o pedido? (1 = sim / 2 = não): "))
-    while (conf != "1" and conf != "2"):
-        conf = int(input("[ERRO] Digite um comando válido (1/2): "))
-
-    if conf == "1":
-        print("Pedido Enviado com sucesso!")
-    else:
-        print("Pedido cancelado!")
-
+ 
+    name = str(input("Digite o seu nome: "))
+    while name == "":
+        name = str(input("[ERRO] Digite um nome válido: "))
+ 
+    while choise == 1:
+ 
+        choose = str(input("Digite o código do produto que deseja comprar: "))
+        ListarADM()
+        while choose not in codigo_produto:
+            choose = str(input("[ERRO] por favor digite um número válido: "))
+ 
+        codigo_choosed = codigo_produto.index(choose)
+        print("\nO produto selecionado tem o código: %s. Descrição: %s. Preço: %s \n" %(lista_produto[codigo_choosed][0], lista_produto[codigo_choosed][1], lista_produto[codigo_choosed][2]))#Como é bom reaproveitar código...Quero só ver na prova :D
+ 
+        quantidade = int(input("Digite a quantidade que deseja do produto: "))
+        while quantidade <= 0:
+            quantidade = int(input("[EROO] Digite uma quantidade VÁLIDA para o produto: "))
+ 
+        realmoney = float(lista_produto[codigo_choosed][2])
+ 
+        despesa = realmoney * quantidade
+ 
+        print("O total será de %0.2f \n" % (despesa))
+ 
+        print("Pedido do Cliente %s: Código: %s.  Descrição: %s. Preço: %s. Total a pagar: %0.2f \n" %(name, lista_produto[codigo_choosed][0], lista_produto[codigo_choosed][1], lista_produto[codigo_choosed][2], despesa))
+ 
+        conf = str(input("Deseja confirmar o pedido? (1 = sim / 2 = não): "))
+        while (conf != "1" and conf != "2"):
+            conf = int(input("[ERRO] Digite um comando válido (1/2): "))
+ 
+        if conf == "1":
+            print("Pedido Enviado com sucesso!")
+            lista_pedido.append([lista_produto[codigo_choosed][0], lista_produto[codigo_choosed][1], lista_produto[codigo_choosed][2], name, f"{despesa}"])
+ 
+            arquivo = open("pedidos.txt", 'a+',  encoding='utf-8')
+            dados = arquivo.readlines()
+ 
+            if dados == []:
+                arquivo.write((lista_pedido[0][0]) + " " + (lista_pedido[0][1]) + " " + (lista_pedido[0][2]) + " " + (lista_pedido[0][3]) + " " + (lista_pedido[0][4]) + "\n")
+                arquivo.close()
+            else:
+                for i in range(len(dados)):
+                    arquivo.write((lista_pedido[0][0]) + " " + (lista_pedido[0][1]) + " " + (lista_pedido[0][2]) + " " + (lista_pedido[0][3]) + " " + (lista_pedido[0][4]) + "\n")
+                arquivo.close()
+ 
+ 
+        else:
+            print("Pedido cancelado!")
+ 
+        repeat = str(input("Deseja reenviar outro pedido? (1 = sim / 2 = não): "))
+        while repeat != "1" and  repeat != "2":
+            repeat = str(input("[ERRO] Digite um código válido (1 - sim / 2 - não): "))
+        if repeat == "1":
+            print("A opção desejada foi de continuar as requisições")
+        else:
+            print("A opção desejada foi de cancelar mais requisições \n")
+            choise = 2
+           
+ 
     print("\n\n")
-
+ 
 #Main
  
 while confirm == "s":
@@ -241,6 +278,7 @@ while confirm == "s":
     elif escolha == "2": #Se escolher operador... #se for 2, modo Operador
         print("A opção desejada foi de entrar como Operador! \n")
         operator()
+ 
     elif escolha == "3": #Se escolher sair...
         print("A opção desejada foi de sair")
         print("\n")
